d496ef53049d1363525073032e5719586067ba6d
Jiyp <razerfans@gmail.com>
2013-06-24 14:51:27 +0800

    home page

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<title>萌影画</title>
+	<meta name="description" content="Hi,欢迎来到萌影画工作室.我们是热爱电影的爱好者.这里有几个我们的作品,希望您喜欢.有什么感受希望您跟我们联系哈">
+	<meta name="author" content="金依萌">
+	<link rel="icon" type="image/ico" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="/public/css/pages/common.css" />
+	<link rel="stylesheet" type="text/css" href="/public/css/pages/home.css"  />
+</head>
+<body>
+<div id="main">
+		<?php include './views/common/header.php'; ?>
+		<div class="brief">
+			<div class="bg"><img src="/public/images/quotation_bg_2.png" style="width:100%;height:584px;" /></div>
+			<div class="content">
+				<h1 class="png_fix">logo</h1>
+				<p style="margin-top:150px;">Hi,欢迎来到萌影画工作室。我们是热爱电影的爱好者。这里有几个我们的作品，</p>
+				<p>希望您喜欢。有什么感受希望您跟我们联系咯哈 :-)</p>
+				<p>-- 金依萌导演 --</p>
+			</div>
+		</div>
+		
+		<div class="my_works">
+			<div class="wrap">
+				<h2 class="title png_fix">我们的作品</h2>
+				<ul class="movie_list clearfix">
+					<li>
+						<img src="/public/images/pic_1_gray_293.png" />
+					</li>
+					<li><img src="/public/images/pic_1_gray_293.png" /></li>
+					<li style="margin-right:0;"><img src="/public/images/pic_1_gray_293.png" /></li>
+					<li><img src="/public/images/pic_1_gray_293.png" /></li>
+					<li><img src="/public/images/pic_1_gray_293.png" /></li>
+					<li style="margin-right:0;"><img src="/public/images/pic_1_gray_293.png" /></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="preview">
+			<h2 class="title png_fix">预告片</h2>
+			<div style="width:930px;margin: 0 auto;padding:72px 0 100px 0;">
+				<embed src="http://player.youku.com/player.php/sid/XNTcwMDEzNjU2/v.swf" allowFullScreen="true" quality="high" width="930" height="498" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+				<!-- JiaThis Button BEGIN -->
+				<div style="float:right;padding-top:24px;">
+				<div class="jiathis_style_32x32" style="height:32px;">
+					<a class="jiathis_button_qzone"></a>
+					<a class="jiathis_button_tsina"></a>
+					<a class="jiathis_button_tqq"></a>
+					<a class="jiathis_button_weixin"></a>
+					<a class="jiathis_button_renren"></a>
+					<a href="http://www.jiathis.com/share" class="jiathis jiathis_txt jtico jtico_jiathis" target="_blank"></a>
+					<a class="jiathis_counter_style"></a>
+				</div>
+				</div>
+				<script type="text/javascript" src="http://v3.jiathis.com/code/jia.js?uid=1368857247128408" charset="utf-8"></script>
+				<!-- JiaThis Button END -->
+			</div>
+		</div>
+		
+		<div class="weibo">
+			<h2 class="title png_fix">微博动态</h2>
+			<div>
+			<iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=1&ptype=1&speed=300&skin=9&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1149989810&verifier=78e1ee4d&dpc=1"></iframe>
+			</div>
+		</div>
+		
+		<?php include './views/common/footer.php'; ?>
+</div>
+<script src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
+<script src="<?=$host?>/public/js/core.js"></script>
+</body>
+</html>
